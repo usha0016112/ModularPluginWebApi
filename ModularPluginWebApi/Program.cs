dfbb6c248daa635015b0277a91dad32d26da225b
naveen vishnu <naveen902575@gmail.com>
2026-03-05 09:55:54 +0530

    Update Program.cs

--- ModularPluginWebApi/Program.cs
+++ ModularPluginWebApi/Program.cs
@@ -6,15 +6,18 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// ✅ Swagger always enabled (no Development condition)
 app.UseSwagger();
+
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "ModularPluginWebApi v1");
-    c.RoutePrefix = string.Empty;   // 👈 THIS makes Swagger load at root "/"
+
+    // index.html maintain panna
+    c.RoutePrefix = "index";
+
+    // click panna URL update aagum
+    c.EnableDeepLinking();
 });
 
 app.MapControllers();
-
-var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
-app.Run($"http://0.0.0.0:{port}");
+app.Run();
